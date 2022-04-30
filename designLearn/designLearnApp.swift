155433712965d9c031cc5b766c8f262e3690b3f4
dpzybalov <77212554+dpzybalov@users.noreply.github.com>
2022-04-30 15:34:37 +0500

    Initial Commit

---
+++ designLearn/designLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  designLearnApp.swift
+//  designLearn
+//
+//  Created by dpzybalov on 30.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct designLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
